ef288978f4f39d8947a783d442810fe2343b94d5
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-03-19 19:26:12 +0900

    feat: implemented hap test

--- examples/hap_test.nim
+++ examples/hap_test.nim
@@ -3,6 +3,7 @@ import tcx_addons
 import consoleUtil
 
 import std/strformat
+import std/strutils
 import nimline
 import cppstl
 
@@ -12,13 +13,13 @@ import cppstl
 
 using namespace trussc;
 using namespace tc;
+using namespace tcx::hap;
 """ .}
 
-# defineCppType(OscSender, "OscSender", "tcxOsc.h")
-# defineCppType(OscMessage, "OscMessage", "tcxOsc.h")
+defineCppType(HapPlayer, "HapPlayer", "tcxHapPlayer.h")
 
-# var osc_sender: OscSender
-# var osc_msg: OscMessage
+var is_loaded = false
+var player: HapPlayer
 
 proc setup() {.cdecl.} =
     discard
@@ -28,10 +29,30 @@ proc setup() {.cdecl.} =
     # discard osc_msg.clear()
 
 proc update() {.cdecl.} =
-    discard
+    let r: float = global.getFrameRate()
+    let s = fmt"{r:.2f}"
+    discard global.setWindowTitle(s)
+
+    if is_loaded and player.isLoaded().to(bool):
+        discard player.update()
+        # echo $player.getCurrentTime().to(float)
 
 proc draw() {.cdecl.} =
-    discard
+    discard global.setColor(1, 1, 1)
+
+    if not is_loaded:
+        discard global.drawBitmapString("drop hap file here", 30, 30)
+    else:
+        if player.isLoaded().to(bool):
+            discard global.resetStyle()
+            discard player.draw(0, 0, global.getWindowWidth(), global.getWindowHeight())
+
+            discard global.setColor(0.3, 0.3, 0.3, 0.5)
+            discard global.drawRect(0, 20, 200, 40)
+
+            discard global.setColor(1, 1, 1)
+            let t = player.getCurrentTime().to(float)
+            discard global.drawBitmapString(fmt"{t:0.2f}", 30, 30)
 
 proc keyPressed(key: cint) {.cdecl.} =
     let ckey = cast[char](key)
@@ -48,6 +69,16 @@ proc filesDropped(info: pointer) {.cdecl.} =
         files.add($f)
 
     echo "dropped files: ", $(files)
+
+    if files.len > 0 and 
+        (files[0].endsWith(".mov") or files[0].endsWith(".avi")):
+        discard player.load(files[0])
+        discard player.play()
+        discard player.setLoop(true)
+
+        is_loaded = true
+    else:
+        echo "[Warning] files dropped, but only .mov and .avi is supported!"
 
 when isMainModule:
     showConsole() # this is necessary to see logs
